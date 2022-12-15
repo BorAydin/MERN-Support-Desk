867581a876d93a82fdf4241069c3947a2ba58a37
BorAydin <boraaydins@gmail.com>
2022-12-15 06:44:52 +0300

    fm

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,11 @@
-console.log('High amk')
+const express = require('express')
+const doteenv = require('dotenv').config()
+const PORT = process.env.PORT || 8000
+
+const app = express()
+
+app.get('/', (reg, res) => {
+  res.status(200).json({message: 'Support Desk API'})
+})
+
+app.listen(PORT, () => console.log(`Server ${PORT} portunda başlatıldı.`))
