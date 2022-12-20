89c50b1de7e137a7fe68ae8e516328c5bed0c0b5
BorAydin <boraaydins@gmail.com>
2022-12-20 04:33:28 +0300

    fm3

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const doteenv = require('dotenv').config()
-const PORT = process.env.PORT || 8000
+const {errorHandler} = require('./middleware/errorMiddleware')
+const PORT = process.env.PORT || 5000
 
 const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false}))
 
 app.get('/', (reg, res) => {
   res.status(200).json({message: 'Support Desk API'})
@@ -10,5 +14,7 @@ app.get('/', (reg, res) => {
 
 // Routes
 app.use('/api/users', require('./routes/userRoutes'))
+
+app.use(errorHandler)
 
 app.listen(PORT, () => console.log(`Server ${PORT} portunda başlatıldı.`))
