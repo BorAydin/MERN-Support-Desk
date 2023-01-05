6c000a7a036725cb21ce19f30e46d2f1c251a4d7
BorAydin <boraaydins@gmail.com>
2023-01-06 02:15:43 +0300

    fm16

--- backend/controllers/ticketController.js
+++ backend/controllers/ticketController.js
@@ -7,14 +7,45 @@ const Ticket = require('../models/ticketModel')
 // route GET /api/tickets
 // @access Private
 const getTickets = asyncHandler(async (req, res) => {
-  res.status(200).json({message: 'getTickets'})
+  // Get user using the id in the JWT
+  const user = await User.findById(req.user.id)
+
+  if(!user) {
+    res.status(401)
+    throw new Error('Kullanıcı bulunamadı.')
+  }
+
+  const tickets = await Ticket.find({user: req.user.id})
+
+  res.status(200).json(tickets)
 })
 
 // @desc Create new a ticket
 // route POST /api/tickets
 // @access Private
 const createTicket = asyncHandler(async (req, res) => {
-  res.status(200).json({message: 'createTickets'})
+  const {product, description} = req.body
+
+  if(!product || !description){
+    res.status(400)
+    throw new Error('Lütfen ürün ve açıklama giriniz.')
+  } 
+
+  const user = await User.findById(req.user.id)
+
+  if(!user) {
+    res.status(401)
+    throw new Error('Kullanıcı bulunamadı.')
+  }
+
+  const ticket = await Ticket.create({
+    product,
+    description,
+    user: req.user.id,
+    status: 'new'
+  })
+
+  res.status(201).json(ticket)
 })
 
 module.exports = {
